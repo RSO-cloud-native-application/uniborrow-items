87e145deabe6e9988d7e1c27319cae4ecac7de50
Miha <mp6079@student.uni-lj.si>
2022-01-07 17:24:20 +0100

    added service discovery

--- api/src/main/java/si/fri/rso/uniborrow/items/api/v1/ItemsApplication.java
+++ api/src/main/java/si/fri/rso/uniborrow/items/api/v1/ItemsApplication.java
@@ -2,7 +2,9 @@ package si.fri.rso.uniborrow.items.api.v1;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 
+@RegisterService(value = "uniborrow-items-service", environment = "dev", version = "1.0.0")
 @ApplicationPath("/v1")
 public class ItemsApplication extends Application {
 
